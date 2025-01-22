3fb7e06ece6202ca344862016a4159131c71d51b
Saba Hussain <saba.mar88@gmail.com>
2025-01-22 09:43:44 +0500

    delete two folders

--- blog-website/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Footer() {
-  return (
-  <footer className="bg-black opacity-80 shadow p-3 text-center">
-    <p className="text-gray-500">© 2025 all rights reserved</p>
-  </footer>
-  )
-}
-
